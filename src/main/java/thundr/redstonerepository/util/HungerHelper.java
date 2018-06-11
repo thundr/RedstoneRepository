af081a36f982c2170799c017fb177a54a3067dec
Renoh47 <renoh47@gmail.com>
2018-06-11 01:58:10 -0700

    HungerHelper implementation

--- src/main/java/thundr/redstonerepository/util/HungerHelper.java
+++ src/main/java/thundr/redstonerepository/util/HungerHelper.java
@@ -2,6 +2,9 @@ package thundr.redstonerepository.util;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.FoodStats;
+import net.minecraft.entity.player.EntityPlayer;
+
 
 public class HungerHelper {
 	public static ItemStack setDefaultHungerTag(ItemStack container) {
@@ -10,5 +13,15 @@ public class HungerHelper {
 			container.getTagCompound().setInteger("Hunger", 0);
 		}
 		return container;
+	}
+
+	public static void addHunger(EntityPlayer player, int amount) {
+		//Only add hunger points, not saturation. This automatically caps at 20 due to addStats logic.
+		player.getFoodStats().addStats(amount, 0.0F);
+	}
+	public static void addSaturation(EntityPlayer player, int amount) {
+		FoodStats foodStats = player.getFoodStats();
+		//Cap saturation at 20 points.
+		foodStats.setFoodSaturationLevel(Math.min(foodStats.getSaturationLevel() + amount, 20.0F));
 	}
 }
