d732393328eb09b007aada31b53f37eb20fcbdd4
THUNDR <thundr@protonmail.ch>
2018-06-11 01:58:07 -0700

    hunger now increments with proper calculations

--- src/main/java/thundr/redstonerepository/gui/GuiFeeder.java
+++ src/main/java/thundr/redstonerepository/gui/GuiFeeder.java
@@ -4,11 +4,18 @@ import cofh.core.gui.GuiContainerCore;
 import cofh.core.gui.element.ElementButton;
 import cofh.core.gui.element.ElementButtonManaged;
 import cofh.core.gui.element.tab.TabInfo;
+import cofh.core.util.helpers.StringHelper;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import org.lwjgl.input.Keyboard;
+import thundr.redstonerepository.RedstoneRepository;
 import thundr.redstonerepository.gui.element.ElementEnergyItem;
 import thundr.redstonerepository.gui.element.ElementHungerPoints;
 import thundr.redstonerepository.init.RedstoneRepositoryProps;
+import thundr.redstonerepository.items.ItemFeeder;
+import thundr.redstonerepository.util.HungerHelper;
 
 public class GuiFeeder extends GuiContainerCore {
 
@@ -18,6 +25,10 @@ public class GuiFeeder extends GuiContainerCore {
 
 	String PATH_BUTTON = RedstoneRepositoryProps.PATH_GUI + "storage_1.png";
 
+	ItemStack feederStack;
+	ItemFeeder baseFeeder;
+	ContainerFeeder containerFeeder;
+
 	public GuiFeeder(InventoryPlayer inventoryPlayer, ContainerFeeder containerFeeder){
 		super(containerFeeder);
 
@@ -26,6 +37,9 @@ public class GuiFeeder extends GuiContainerCore {
 		xSize = 14 + 18 * 9;
 		ySize = 112 + 18 * 2;
 		texture = RedstoneRepositoryProps.FEEDER_GUI_STORAGE;
+		this.feederStack = containerFeeder.getContainerStack();
+		this.baseFeeder = (ItemFeeder) containerFeeder.getContainerStack().getItem();
+		this.containerFeeder = containerFeeder;
 
 		generateInfo("tab.redstonerepository.feeder");
 	}
@@ -50,15 +64,29 @@ public class GuiFeeder extends GuiContainerCore {
 	}
 
 	@Override
-	public void handleElementButtonClick(String button, int mouseButton){
-		if(mouseButton == 0){
-			//eat one food item from slot
-		}
-		else if(mouseButton == 1){
-			//eat all food in slot
-		}
+	protected void drawGuiContainerForegroundLayer(int x, int y) {
 
-		playClickSound(0.7F);
+		if (drawTitle & name != null) {
+			fontRenderer.drawString(StringHelper.localize(name), 6, 6, 0x404040);
+		}
+		if (drawInventory) {
+			fontRenderer.drawString(StringHelper.localize("container.inventory"), 8, ySize - 96 + 3, 0x404040);
+		}
+		drawElements(0, true);
+		drawTabs(0, true);
 	}
 
+	@Override
+	public void handleElementButtonClick(String button, int mouseButton){
+		ItemStack tmpStack = containerFeeder.inventorySlots.get(containerFeeder.inventorySlots.size() - 1).getStack().copy();
+
+		if(mouseButton == 0){
+			tmpStack.setCount(1);
+			baseFeeder.receiveHungerPoints(feederStack, HungerHelper.findHungerValues(tmpStack));
+		}
+		else if(mouseButton == 1){
+			baseFeeder.receiveHungerPoints(feederStack, HungerHelper.findHungerValues(tmpStack));
+		}
+		playClickSound(0.7F);
+	}
 }
--- src/main/java/thundr/redstonerepository/util/HungerHelper.java
+++ src/main/java/thundr/redstonerepository/util/HungerHelper.java
@@ -1,5 +1,6 @@
 package thundr.redstonerepository.util;
 
+import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
@@ -10,5 +11,15 @@ public class HungerHelper {
 			container.getTagCompound().setInteger("Hunger", 0);
 		}
 		return container;
+	}
+
+	//returns the value of an ItemStack of food in Hunger Points
+	//each point of food and saturation is a hunger point
+	public static int findHungerValues(ItemStack food){
+		if(!(food.getItem() instanceof ItemFood)){
+			return 0;
+		}
+		ItemFood itemFood = (ItemFood) food.getItem();
+		return (int) (food.getCount() * ((itemFood.getHealAmount(food) * itemFood.getSaturationModifier(food) * 2.0F) + itemFood.getHealAmount(food)));
 	}
 }
