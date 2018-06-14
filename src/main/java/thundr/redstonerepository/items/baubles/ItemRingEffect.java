296ff84b54c38ed27ad7964cce13133e9e8d1344
Renoh47 <renoh47@gmail.com>
2018-06-14 04:22:50 -0700

    stasis ring work

--- src/main/java/thundr/redstonerepository/items/baubles/ItemRingEffect.java
+++ src/main/java/thundr/redstonerepository/items/baubles/ItemRingEffect.java
@@ -2,12 +2,18 @@ package thundr.redstonerepository.items.baubles;
 
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
+import baubles.common.Baubles;
 import cofh.core.util.CoreUtils;
+import cofh.core.util.helpers.BaublesHelper;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.PotionColorCalculationEvent;
 import net.minecraftforge.fml.common.Optional;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import thundr.redstonerepository.RedstoneRepository;
 import thundr.redstonerepository.items.ItemCoreRF;
 
@@ -30,7 +36,6 @@ public class ItemRingEffect extends ItemCoreRF implements IBauble {
 		if(!(player instanceof EntityPlayer) || player.world.isRemote || CoreUtils.isFakePlayer(player)){
 			return;
 		}
-
 		EntityPlayer entityPlayer = (EntityPlayer)player;
 
 		if(isActive(ring) && (getEnergyStored(ring) >= getEnergyPerUse(ring))){
@@ -46,5 +51,18 @@ public class ItemRingEffect extends ItemCoreRF implements IBauble {
 	    }
 
 
+
+	}
+
+	@SubscribeEvent
+	public void potionListener(PotionColorCalculationEvent event){
+    	if (event.getEntity() instanceof EntityPlayer){
+    		EntityPlayer player = (EntityPlayer)event.getEntity();
+    		for (ItemStack item : BaublesHelper.getBaubles(player)){
+    			if (item.getItem() instanceof ItemRingEffect){
+					RedstoneRepository.LOG.info("RingEffect: Caught potion event on player wearing ring");
+				}
+			}
+		}
 	}
 }
